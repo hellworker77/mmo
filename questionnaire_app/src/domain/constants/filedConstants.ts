14f66662ee0e0cd823b745f7b4cc0a98b25fae96
Ilya <iljapuskov@gmail.com>
2024-05-31 19:09:45 +0200

    feat: enums and constants

---
+++ questionnaire_app/src/domain/constants/filedConstants.ts
@@ -0,0 +1,4 @@
+export const INNLength = 10;
+export const OGRNLength = 15;
+export const OGRNIPLength = 15;
+export const DateLength = 8;
---
+++ questionnaire_app/src/domain/enums/kindOfActivity.ts
@@ -0,0 +1,4 @@
+export enum KindOfActivity{
+    IndividualEntrepreneur,
+    LimitedLiabilityCompany
+}
