4670b899f85633eab4c032965e62a89f5d7cd4bc
Ilya <iljapuskov@gmail.com>
2024-05-31 15:36:03 +0200

    feat: Shared

---
+++ server/Shared/Constants/CacheConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace Shared.Constants;
+
+public static class CacheConstants
+{
+    private const string RestrictedStatusCacheKey = "restrictedStatus";
+    public static readonly TimeSpan CacheSlidingExpirationTime = TimeSpan.FromHours(24);
+    public static string GetRestrictedUserCacheKey(Guid userId) => $"{RestrictedStatusCacheKey}:{userId}";
+}
